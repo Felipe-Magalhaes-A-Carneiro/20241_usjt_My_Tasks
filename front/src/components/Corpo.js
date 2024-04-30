3486b3c9041ab4eddd592bc8dd62d2e4a4deb892
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-04-29 21:48:25 -0300

    update(package.jason): incrementa a dependencia mysql

--- front/src/components/Corpo.js
+++ front/src/components/Corpo.js
@@ -10,18 +10,23 @@ const comecarAgora = () =>{
 const Corpo = () => {
   //rafce = atalho feito pela extenção React ES7 + React/
   return (
+
     <div className='d-inline-block marginBody'>
       <div className='text-center vw-100 vh-100'>
+
         <h3 className=' fw-bold p-3 mb-2 bg-light text-dark rounded widthTitle'>
           Aprenda Programação com ChatGPT
         </h3>
+
         <br />
         <span className=' p-2 mb-2 bg-dark rounded'>
           <a className='text-white text-decoration-none'>
             Faça perguntas, salve respostas e faça anotações!</a>
         </span>
         <br />
+
         <br />
+        
         <button type='button' className='btn bg-verde btn-lg rounded-pill fw-bold border border-3 border-black' 
                 onClick={comecarAgora}> Começe agora! </button>
       </div>
