04cf065abbe67baad970c55246615e7eeb9cda30
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-05-21 16:02:59 -0300

    fix(index.js): corrigi conexao da api com o banco de dados

--- back/mysql/script.sql
+++ back/mysql/script.sql
@@ -1,4 +1,4 @@
--- criar a base
+-- criar a database
 create database if not exists mytasks;
 -- ou
 create database mytasks;
@@ -6,14 +6,32 @@ create database mytasks;
 -- informar ao MySQL qual base sobre a qual use schemas usar;
 use mytasks;
 
+-- criar tabela para usuario
+-- create table if not exists tb_usuario(
+	-- id int primary key,
+    -- nome varchar(200) not null,
+    -- email varchar(50) not null,
+    -- senha varchar(50) not null
+-- );
+
 -- criar tabela de perguntas
 create table if not exists tb_perguntas(
-	id_pergunta int primary key,
+	id_pergunta int primary key auto_increment,
     pergunta varchar(200)
 );
 
 -- criar tabela de respostas
 create table if not exists tb_respostas(
-	id_respota int primary key,
-    resposta varchar(200)
+	id_respota int primary key auto_increment,
+    id_pergunta int,
+    resposta varchar(200),
+    foreign key (id_pergunta) references tb_perguntas(id_pergunta)
 );
+
+-- visualizar tabela:
+-- select * from tb_usuario;
+-- visualizar tabela tb_perguntas:
+select * from tb_perguntas;
+
+-- visualizar tabela tb_respostas:
+select * from tb_respostas;
